647cd9c0875859f1958af1c8e18498c49f00a6eb
choewy <choewy32@gmail.com>
2023-09-24 18:28:31 +0900

    feature/rpush with idx

--- src/module/board/socket-board/socket-board.tsx
+++ src/module/board/socket-board/socket-board.tsx
@@ -30,11 +30,12 @@ export const SocketBoard: FC<{ mode: BoardMode }> = ({ mode }) => {
     };
   }, [mode, numberHandler, stringHandler, objectHandler]);
 
-  const load = mode === BoardMode.SOCKET && socket.connected;
+  const load = mode === BoardMode.SOCKET;
+
   const emptyText =
-    socket.connected === true
-      ? 'loading...'
-      : 'socket.io disconnected.\nserver is closed.\nenter `npm run server` in other terminal.';
+    socket.connected === false
+      ? 'socket.io disconnected.\nserver is closed.\nenter `npm run server` in other terminal.'
+      : 'loading...';
 
   return (
     <ContainerComponent columeCount={3}>
